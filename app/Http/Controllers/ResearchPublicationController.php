02da6b771e7f9713f37b2833bd784f3d8c9538ce
Azis202017 <azisa6980@gmail.com>
2024-01-31 11:25:51 +0700

    Research Publication

--- app/Http/Controllers/ResearchPublicationController.php
+++ app/Http/Controllers/ResearchPublicationController.php
@@ -9,20 +9,39 @@ class ResearchPublicationController extends Controller
 {
     public function index()
     {
-        return ResearchPublication::with(['research_area'])->get();
+        $data = ResearchPublication::with(['research_area'])->get();
+
+        $research = $data->map(function ($research_data) {
+            $research_data->foto_url = asset('research/' . $research_data->foto);
+            $research_data->research_area->icon_url = asset('icon/' . $research_data->research_area->icon);
+            $research_data->research_area->foto_url = asset('foto/' . $research_data->research_area->foto);
+
+            return $research_data;
+        });
+
+        return response()->json([
+            'data' => $research,
+        ]);
     }
-    public function show($id) {
+
+    public function show($id)
+    {
         $publication = ResearchPublication::find($id);
         return $publication;
     }
     public function create(Request $request)
     {
+        $uploadedFile = $request->file('foto');
+        $imageName = uniqid() . '.' . $uploadedFile->getClientOriginalExtension();
+
+        $uploadedFile->move(public_path('research'), $imageName);
         $data = ResearchPublication::create([
             'id_research_area' => $request->id_research_area,
             'title' => $request->title,
             'description' => $request->description,
             'link_publication' => $request->link_publication,
             'publish_year' => $request->publish_year,
+            'foto' => $imageName
         ]);
         return response()->json(['data' => $data, 'status' => 'OK'], 200);
     }
@@ -37,9 +56,10 @@ class ResearchPublicationController extends Controller
         $data->save();
         return response()->json(['data' => $data, 'status' => 'OK'], 200);
     }
-    public function delete($id) {
+    public function delete($id)
+    {
         $data = ResearchPublication::find($id);
         $data->delete();
-        return response()->json(['data' => $data,'status' => 'OK'], 200);
+        return response()->json(['data' => $data, 'status' => 'OK'], 200);
     }
 }
