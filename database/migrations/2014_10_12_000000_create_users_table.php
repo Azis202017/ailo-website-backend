e97a1642cecaf16598b0d6929c5400e36f413554
Azis202017 <azisa6980@gmail.com>
2024-01-31 17:40:22 +0700

    Update users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,19 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-
+            $table->string('kode_asistant')->nullable();
+            $table->string('jurusan')->nullable();
+            $table->string('tahun_asistant')->nullable();
+            $table->string('foto')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('github')->nullable();
+            $table->string('discord')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('jabatan')->nullable();
+            $table->string('kategori_asistant')->nullable();
+            $table->enum('is_assistant',['dosen','mahasiswa'])->nullable();
+            $table->string('biography')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
