a2b3b6546ab2d040c2db34ad080f44bff44a2544
Azis202017 <azisa6980@gmail.com>
2024-01-31 17:39:55 +0700

    User Expertise

--- app/Http/Controllers/UserResearchPublicationController.php
+++ app/Http/Controllers/UserResearchPublicationController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserResearchPublication;
 use Illuminate\Http\Request;
 
 class UserResearchPublicationController extends Controller
 {
-    //
+    public function create(Request $request) {
+        $userResearchPublication = new UserResearchPublication();
+        $userResearchPublication->title = $request->title;
+        $userResearchPublication->id_user = $request->id_user;
+        $userResearchPublication->save();
+        return $userResearchPublication;
+    }
 }
