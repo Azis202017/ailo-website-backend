45f9462aa5587e8b6aea0b097a44c4395bd16ccb
Azis202017 <azisa6980@gmail.com>
2024-01-31 07:17:19 +0700

    Research area dan research publication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\PatentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatentController;
+use App\Http\Controllers\ResearchAreaController;
+use App\Http\Controllers\ResearchPublicationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +22,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/patents', [PatentController::class,'index']);
-Route::post('/patents/create', [PatentController::class,'store']);
-Route::post('/patents/{id}', [PatentController::class,'edit']);
-Route::delete('/patents/delete/{id}', [PatentController::class,'delete']);
 
+Route::get('/patents', [PatentController::class, 'index']);
+Route::post('/patents/create', [PatentController::class, 'store']);
+Route::post('/patents/{id}', [PatentController::class, 'edit']);
+Route::delete('/patents/delete/{id}', [PatentController::class, 'delete']);
+Route::get('/research-area', [ResearchAreaController::class, 'index']);
 
+Route::post('/research-area/create', [ResearchAreaController::class, 'create']);
+Route::post('/research-area/edit/{id}', [ResearchAreaController::class, 'edit']);
 
+Route::delete('/research-area/delete/{id}', [ResearchAreaController::class, 'delete']);
+
+Route::get('/research-publication', [ResearchPublicationController::class, 'index']);
+Route::get('/research-publication/detail/{id}', [ResearchPublicationController::class, 'index']);
+
+Route::post('/research-publication/create', [ResearchPublicationController::class, 'create']);
+Route::post('/research-publication/edit/{id}', [ResearchPublicationController::class, 'edit']);
+
+Route::delete('/research-publication/delete/{id}', [ResearchPublicationController::class, 'delete']);
