9b0f0f0646dc7ef7f9d8d022c8732cbb9fd4ed2f
Azis202017 <azisa6980@gmail.com>
2024-02-06 11:21:44 +0700

    User Expertise

--- app/Http/Controllers/UserExpertiseController.php
+++ app/Http/Controllers/UserExpertiseController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserExpertise;
+use Illuminate\Support\Facades\Auth;
 
 class UserExpertiseController extends Controller
 {
     public function create(Request $request) {
+        $user  = Auth::user();
         $userExpertise = new UserExpertise();
+
         $userExpertise->title = $request->title;
-        $userExpertise->id_user = $request->id_user;
+        $userExpertise->id_user = $user->id;
         $userExpertise->save();
         return $userExpertise;
     }
